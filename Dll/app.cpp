718e402985726df66ec6350734c7f248a5dcd318
SqueezyLemon <19240486+SqueezyLemon@users.noreply.github.com>
2020-12-27 00:07:43 +0100

    #3, #4 intercepting packets and sending them to gui

--- Dll/app.cpp
+++ Dll/app.cpp
@@ -1,21 +1,54 @@
-#include <Windows.h>
+#include "outputDevice/namedPipe.hpp"
+#include "outputDevice/console.hpp"
 #include "packetLogger.hpp"
 
-void InitConsole()
+#include <Windows.h>
+#include <thread>
+
+static bool shouldExit = false;
+
+BOOL WINAPI ConsoleCtrlHandler(DWORD signal)
 {
-	AllocConsole();
-	FILE *stream;
-	freopen_s(&stream, "CONOUT$", "w", stdout);
+	if(signal == CTRL_BREAK_EVENT)
+	{
+		std::cout << "Ctrl-Break pressed, exiting...\n";
+		shouldExit = true;
+	}
+
+	return TRUE;
 }
 
-void OnAttach()
+void OnAttach(HINSTANCE hInstance)
 {
-	InitConsole();
-	puts("packet logger injected");
+	auto console = std::make_shared<Console>(Console::StreamType::STDOUT);
+	//auto tcpSocket = std::make_shared<TcpSocket>(ioContext);
+	auto namedPipe = std::make_shared<NamedPipe>();
+	auto &packetLoggerContext = PacketLogger::PacketLoggerContext::GetInstance();
 
-	PacketLogger::Init();
-	PacketLogger::Start();
-	//Free console and exit thread
+	std::cout << "Setting Ctrl-Break exit handler ";
+	BOOL isConsoleHandlerAdded = SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE);
+	puts((isConsoleHandlerAdded ? "OK" : "!OK"));
+
+	puts("Checking GUI connection");
+	if(namedPipe->Open(L"\\\\.\\pipe\\test_pipe"))
+	{
+		puts("Packets will be sent to GUI");
+		packetLoggerContext.Start(namedPipe);
+	}
+	else
+	{
+		puts("Packets will be sent to Console");
+		packetLoggerContext.Start(console);
+	}
+
+	while(!shouldExit)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+
+	packetLoggerContext.Stop();
+	puts("Cleaned");
+	FreeLibrary(hInstance);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpvReserved)
@@ -23,7 +56,7 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpvReserved)
 	if(dwReason == DLL_PROCESS_ATTACH)
 	{
 		DisableThreadLibraryCalls(hInstance);
-		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)OnAttach, NULL, NULL, NULL);
+		std::thread(OnAttach, hInstance).detach();
 	}
 
 	return TRUE;
--- Dll/packetLogger.hpp
+++ Dll/packetLogger.hpp
@@ -1,18 +1,160 @@
 #pragma once
 
 #include "tools/process.hpp"
+#include "tools/memory.hpp"
+#include "outputDevice/outputDevice.hpp"
 
+#include <detours/detours.h>
+#include <array>
+#include <memory>
 #include <iostream>
 
 namespace PacketLogger
 {
-	void Init()
+	constexpr auto SEND_PACKET_PATTERN = std::to_array<uint8_t>({0x53, 0x56, 0x8B, 0xF2, 0x8B, 0xD8, 0xEB, 0x04});
+	constexpr auto RECEIVE_PACKET_PATTERN = std::to_array<uint8_t>({0x55, 0x8B, 0xEC, 0x83, 0xC4, 0xF4, 0x53, 0x56, 0x57, 0x33, 0xC9, 0x89, 0x4D, 0xF4, 0x89, 0x55, 0xFC, 0x8B, 0xD8, 0x8B, 0x45, 0xFC});
+
+	void HookedSendPacketWrapper();
+	void HookedReceivePacketWrapper();
+	
+	class PacketLoggerContext
 	{
-		auto mainModuleInfo = Tools::Process::GetModuleInfo();
+	public:
+		friend void HookedSendPacketWrapper();
+		friend void HookedReceivePacketWrapper();
+
+		PacketLoggerContext(PacketLoggerContext const&) = delete;
+		void operator=(PacketLoggerContext const&) = delete;
+
+		static PacketLoggerContext& GetInstance()
+		{
+			static PacketLoggerContext instance;
+			return instance;
+		}
+
+		void Start(const std::shared_ptr<OutputDevice> &outputDevice)
+		{
+			this->outputDevice = outputDevice;
+			auto mainModuleInfo = Tools::Process::GetModuleInfo();
+
+			fncSendPacket = Tools::Memory::FindBytePattern(SEND_PACKET_PATTERN, (size_t)mainModuleInfo.lpBaseOfDll, mainModuleInfo.SizeOfImage);
+			fncReceivePacket = Tools::Memory::FindBytePattern(RECEIVE_PACKET_PATTERN, (size_t)mainModuleInfo.lpBaseOfDll, mainModuleInfo.SizeOfImage);
+
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourAttach((void **)&fncSendPacket, HookedSendPacketWrapper);
+			DetourAttach((void **)&fncReceivePacket, HookedReceivePacketWrapper);
+			DetourTransactionCommit();
+		}
+
+		void Stop()
+		{
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourDetach((void **)&fncSendPacket, HookedSendPacketWrapper);
+			DetourDetach((void **)&fncReceivePacket, HookedReceivePacketWrapper);
+			DetourTransactionCommit();
+			outputDevice.reset();
+		}
+
+	private:
+		PacketLoggerContext() {}
+
+		void HookedSendPacket(const char *packet)
+		{
+			std::string msg(packet);
+			outputDevice->Write("SEND " + msg + '\n');
+		}
+
+		void HookedReceivePacket(const char *packet)
+		{
+			std::string msg(packet);
+			outputDevice->Write("RECV " + msg);
+		}
+
+	private:
+		std::shared_ptr<OutputDevice> outputDevice;
+		void *fncSendPacket = nullptr;
+		void *fncReceivePacket = nullptr;
+	};
+
+	
+	/*
+		Delphi (Borland fastcall)
+		eax = this
+		[edx] = packet
+		[edx-4] = length
+		[edx-8] = ref count
+	*/
+	__declspec(naked) void HookedSendPacketWrapper()
+	{
+		_asm
+		{
+			push ebp
+			mov	ebp, esp
+			sub esp, __LOCAL_SIZE
+		}
+
+		void* _this;
+		char *packet;
+
+		PacketLoggerContext *context;
+		void *fncSendPacket;
+
+		_asm
+		{
+			mov [_this], eax
+			mov [packet], edx
+		}
+		
+		context = &PacketLoggerContext::GetInstance();
+		fncSendPacket = context->fncSendPacket;
+		context->HookedSendPacket(packet);
+		
+		_asm
+		{	
+			mov eax, [_this]
+			mov edx, [packet]
+			call [fncSendPacket]
+			mov	esp, ebp
+			pop	ebp
+			ret
+		}
 	}
 
-	void Start()
+	__declspec(naked) void HookedReceivePacketWrapper()
 	{
-		
+		_asm
+		{
+			push ebp
+			mov	ebp, esp
+			sub esp, __LOCAL_SIZE
+		}
+
+		void* _this;
+		char *packet;
+
+		PacketLoggerContext *context;
+		void *fncSendPacket;
+
+		_asm
+		{
+			mov[_this], eax
+			mov[packet], edx
+		}
+
+		context = &PacketLoggerContext::GetInstance();
+		fncSendPacket = context->fncReceivePacket;
+		context->HookedReceivePacket(packet);
+
+		_asm
+		{
+			mov eax, [_this]
+			mov edx, [packet]
+			call[fncSendPacket]
+			mov	esp, ebp
+			pop	ebp
+			ret
+		}
 	}
 }
