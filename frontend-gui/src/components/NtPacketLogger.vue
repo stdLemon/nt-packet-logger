768db8e9c017cc1f772fe84c56c5abba6d264fee
SqueezyLemon <19240486+SqueezyLemon@users.noreply.github.com>
2021-01-11 15:46:24 +0100

    #5 gui refactoring

---
+++ frontend-gui/src/components/NtPacketLogger.vue
@@ -0,0 +1,54 @@
+<template>
+  <PacketList :packets="packets" />
+</template>
+
+<script>
+import PacketList from "./PacketList";
+import net from "net";
+
+export default {
+  name: "NtPacketLogger",
+
+  components: {
+    PacketList,
+  },
+
+  data() {
+    return {
+      packets: [],
+      cp1250Decoder: new TextDecoder("cp1250")
+    };
+  },
+
+  destroyed() {
+    this.server.close();
+  },
+
+  mounted() {
+    const pipeName = "\\\\.\\pipe\\nt_packet_logger";
+    window.packets = this.packets;
+
+    this.server = net.createServer();
+    this.server.maxConnections = 1;
+
+    this.server.listen(pipeName);
+
+    this.server.on("connection", (socket) => {
+      socket.on("data", (data) => {
+        const decodedPackets = this.cp1250Decoder.decode(data);
+        const newPackets = decodedPackets.split("\n");
+        newPackets.pop();
+        newPackets.forEach((packetStr) => {
+          const tokens = packetStr.split(" ");
+          const [direction, type] = tokens.slice(0, 2);
+          const args = tokens.slice(2).join(" ");
+
+          const packet = { direction, type, args };
+
+          this.packets.push(packet);
+        });
+      });
+    });
+  },
+};
+</script>
